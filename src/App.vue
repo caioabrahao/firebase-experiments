a6b0a0294bc1ec94539e1c1928b16dcf6981d9e0
Caio Abrahão <contato.caioabrahao@gmail.com>
2025-01-12 23:43:13 -0300

    README update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+const user = ref(null);
+
+onMounted(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) {
+            user.value = currentUser.displayName || currentUser.email; // Use displayName if available, otherwise use email
+        } else {
+            user.value = null;
+        }
+    });
+});
 </script>
 
 <template>
+    <p>Logged As: {{ user }}</p>
     <nav>
         <router-link to="/">Home</router-link> |
         <router-link to="/feed">Feed</router-link> |
